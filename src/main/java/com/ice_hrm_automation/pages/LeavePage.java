7b2eb193f410ac64db55e5d97140ebf34efe48b0
rohan309 <rohanmore309@gmail.com>
2023-04-05 11:12:17 +0530

    test added for leave module

---
+++ src/main/java/com/ice_hrm_automation/pages/LeavePage.java
@@ -0,0 +1,7 @@
+package com.ice_hrm_automation.pages;
+
+import com.icecrm_automation.utility.BaseClass;
+
+public class LeavePage extends BaseClass{
+
+}
---
+++ src/test/java/com/icecrm_automation/LeaveTest.java
@@ -0,0 +1,12 @@
+package com.icecrm_automation;
+
+import org.testng.annotations.Test;
+
+public class LeaveTest {
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
